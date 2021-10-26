b0e66f8f7ff5d53f46925e303d15f482865d151d
Harp Sybassion <hsybassi@am-l3.msk.21-school.ru>
2021-10-26 18:47:08 +0300

    uff

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hsybassi <hsybassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 21:17:32 by hsybassi          #+#    #+#             */
-/*   Updated: 2021/10/26 18:39:51 by hsybassi         ###   ########.fr       */
+/*   Updated: 2021/10/26 18:45:51 by hsybassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*string;
 	size_t	i;
-	size_t	s_len;
 	size_t	size;
 
 	s_len = 0;
@@ -24,16 +23,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	size = ft_strlen(s);
 	if (start > size)
-		s_len = 0;
+		len = 0;
 	else if (start + len > size)
-		s_len = size - start;
-	else
-		s_len = len;
-	string = (char *)malloc(sizeof(char) * (s_len + 1));
+		len = size - start;
+	string = (char *)malloc(sizeof(char) * (len + 1));
 	if (!string)
 		return (NULL);
 	i = 0;
-	while (i < s_len)
+	while (i < len)
 	{
 		string[i] = s[i + start];
 		i++;
